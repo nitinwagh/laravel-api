8f92311690e15b2c396b6b9c143ab581b338f8ad
Nitin Wagh <nitin.wagh@fdsinfotech.com>
2018-06-18 17:21:59 +0530

    [TASK] login, register, detail and logout with laravel passport

---
+++ app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\User;
+use Illuminate\Support\Facades\Auth;
+use Validator;
+use Lcobucci\JWT\Parser;
+
+class UserController extends Controller
+{
+    public $successStatus = 200;
+
+    /**
+     * login Api
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function login()
+    {
+        if (Auth::attempt(['email' => request('email'), 'password' => request('password')])) {
+            $user = Auth::user();
+            $success['token'] = $user->createToken('MyApp')->accessToken;
+            return response()->json(['status' => true, 'data' => $success]);
+        } else {
+            return response()->json(['status' => false, 'message' => 'Unauthorised']);
+        }
+    }
+
+    public function logout(Request $request)
+    {
+        $value = $request->bearerToken();
+        $id = (new Parser())->parse($value)->getHeader('jti');
+        $token = $request->user()->tokens->find($id);
+        $token->revoke();
+        return response()->json(['status' => true, 'message' => $this->successStatus]);
+    }
+
+    /**
+     * Register Api
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+                    'name' => 'required',
+                    'email' => 'required|email',
+                    'password' => 'required',
+                    'c_password' => 'required|same:password',
+        ]);
+        if ($validator->fails()) {
+            return response()->json(['status' => false, 'message' => $validator->errors()]);
+        }
+        $input = $request->all();
+        $input['password'] = bcrypt($input['password']);
+        $user = User::create($input);
+        $success['token'] = $user->createToken('MyApp')->accessToken;
+        $success['name'] = $user->name;
+        return response()->json(['status' => true, 'data' => $success, 'message' => $this->successStatus]);
+    }
+
+    /**
+     * details Api
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function details()
+    {
+        $user = Auth::user();;
+         return response()->json(['status' => true, 'data' => $user, 'message' => $this->successStatus]);
+    }
+
+}
